380f3ea7c8ae0b3cdb3a811447d5802f0e0169cb
Kahvipuu <partaar@hotmail.com>
2020-03-21 01:49:19 +0200

    checkstyle

--- src/main/java/minesweeper/bot/TiraBot.java
+++ src/main/java/minesweeper/bot/TiraBot.java
@@ -9,27 +9,31 @@ import java.util.ArrayList;
 import minesweeper.model.Board;
 import minesweeper.model.GameStats;
 import minesweeper.model.Move;
+
+/*
 import minesweeper.model.MoveType;
 import minesweeper.model.Highlight;
 import minesweeper.model.Pair;
 import minesweeper.model.Square;
-
-
-public class TiraBot implements Bot{
+ */
+public class TiraBot implements Bot {
 
     @Override
     public Move makeMove(Board board) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
+        //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public ArrayList<Move> getPossibleMoves(Board board) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
+        //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void setGameStats(GameStats gameStats) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
+        //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
